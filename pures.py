b9ce928d4dde437f76cf372e8b70b6cbdfaca776
Martín Gaitán <gaitan@phasety.com>
2013-08-29 19:17:47 -0300

    basic database

--- pures.py
+++ pures.py
@@ -1,24 +1,32 @@
 from peewee import *
 
-database = SqliteDatabase('dev.db', **{})
+db = SqliteDatabase('dev.db', **{})
 
 
-class Compound(BaseModel):
+class Compound(Model):
+    name = CharField(unique=True)
     formula = CharField()
-    formula_extended = TextField(null=True)
-    name = CharField()
-
+    formula_extended = CharField(null=True)
     pc = FloatField()
     tc = FloatField()
     vc = FloatField()
-
-    weight = FloatField(null=True)
-
-    a = FloatField(null=True)
     acentric_factor = FloatField(null=True)
+    weight = FloatField(null=True)
+    a = FloatField(null=True)
     b = FloatField(null=True)
     c = FloatField(null=True)
     d = FloatField(null=True)
 
+
+    def __unicode__(self):
+        return self.name
+
     class Meta:
+        database = db
         db_table = 'compound'
+
+
+class Alias(Model):
+    compound = ForeignKeyField(Compound, related_name='alias')
+    alias = CharField(unique=True)
+
