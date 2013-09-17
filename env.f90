24df9c027ad86314f38ea329dcdac0fc6683134a
Martín Gaitán <gaitan@phasety.com>
2013-09-17 18:42:16 -0300

    updated and generalized fortran interface

--- env.f90
+++ env.f90
@@ -1,71 +1,90 @@
-module envelope
 
-contains
+module envelope_sur
 
-subroutine rkpr(comp, tc, pc, ohm, ac, b, del, k, K0, Tstar, Lij, n, Tenv, Penv, Denv, n_out, Tcri, Pcri, Dcri, n_cri)
-    implicit none
+    integer, parameter :: PR=1, SRK=2, RKPR=3
+    save
 
-    ! pure compound constants
-    real, dimension(n), intent(in) :: comp
-    real, dimension(n), intent(in) :: tc
-    real, dimension(n), intent(in) :: pc
-    real, dimension(n), intent(in) :: ohm
+    contains
 
-    ! rkpr parameters
-    real, dimension(n), intent(in) :: ac
-    real, dimension(n), intent(in) :: b
-    real, dimension(n), intent(in) :: del
-    real, dimension(n), intent(in) :: k
+    subroutine envelope(nmodel, n, z, tc, pc, ohm, ac, b, del, k_o_m, K0, Tstar, Lij, &
+                    n_points, Tv, Pv, Dv, n_cri, Tcri, Pcri, Dcri)
 
-    ! interaction parameters matrices
-    real, dimension(n,n), intent(in) :: K0
-    real, dimension(n,n), intent(in) :: Tstar
-    real, dimension(n,n), intent(in) :: Lij
+        implicit none
 
-    ! number of compounds in the system
-    integer, intent(in) :: n
+        ! number of compounds in the system
+        integer, intent(in) :: nmodel, n
 
-    ! T, P and Density of the calculated envelope
-    real, dimension(5000), intent(out) :: Tenv
-    real, dimension(5000), intent(out) :: Penv
-    real, dimension(5000), intent(out) :: Denv
+        ! pure compound constants
+        real*8, dimension(n), intent(in) :: z
+        real*8, dimension(n), intent(in) :: tc
+        real*8, dimension(n), intent(in) :: pc
+        real*8, dimension(n), intent(in) :: ohm
 
-    ! number of valid elements in To, Po and Do arrays
-    integer, intent(out) :: n_out
+        ! rkpr parameters
+        real*8, dimension(n), intent(in) :: ac
+        real*8, dimension(n), intent(in) :: b
+        real*8, dimension(n), intent(in) :: del
+        real*8, dimension(n), intent(in) :: k_o_m
 
-    ! T, P and Density of critical points
-    real, dimension(10), intent(out) :: Tcri
-    real, dimension(10), intent(out) :: Pcri
-    real, dimension(10), intent(out) :: Dcri
+        ! interaction parameters matrices
+        real*8, dimension(n,n), intent(in) :: K0
+        real*8, dimension(n,n), intent(in) :: Tstar
+        real*8, dimension(n,n), intent(in) :: Lij
 
-    ! number of valid elements in Tcri, Pcri and Dcri arrays
-    integer, intent(out) :: n_cri
+        ! T, P and Density of the calculated envelope
+        real*8, dimension(5000), intent(out) :: Tv
+        real*8, dimension(5000), intent(out) :: Pv
+        real*8, dimension(5000), intent(out) :: Dv
 
-    ! auxiliar/internal variables
-    integer :: i, j
+        ! number of valid elements in To, Po and Do arrays
+        integer, intent(out) :: n_points
 
-    !-----------------------------------------------------------
-    ! Algorithm starts here :)
+        ! T, P and Density of critical points
+        real*8, dimension(10), intent(out) :: Tcri
+        real*8, dimension(10), intent(out) :: Pcri
+        real*8, dimension(10), intent(out) :: Dcri
 
-    do i=1,n
-        Tenv(i) = 2.0 * tc(i)
-        Penv(i) = 2.0 * pc(i)
-        Denv(i) = 2.0 * ohm(i)
-    end do
+        ! number of valid elements in Tcri, Pcri and Dcri arrays
+        integer, intent(out) :: n_cri
 
-    n_out = n
+        ! auxiliar/internal variables
+        integer :: i, j
 
-    do i=1,3
-        Tcri(i) = Tenv(i)
-        Pcri(i) = Penv(i)
-        Dcri(i) = Denv(i)
-    end do
+        !-----------------------------------------------------------
+        ! Algorithm starts here :)
+        ! this is just dummy code
 
-    n_cri = 3
+        if (nmodel == RKPR) then
+            print *, 'hello RKPR!'
+        else if (nmodel == PR) then
+            print *, 'hi PR!'
+        else if (nmodel == SRK) then
+            print *, 'how u doing SRK!'
+        else
+            print *, 'unknow MODEL'
+            stop 9
+        end if
 
 
-    ! Algorithm ends here
-    !-----------------------------------------------------------
+        do i=1,n
+            Tv(i) = 2.0 * tc(i)
+            Pv(i) = 2.0 * pc(i)
+            Dv(i) = 2.0 * ohm(i)
+        end do
 
-end subroutine rkpr
-end module envelope
+        n_points = n
+
+        do i=1,3
+            Tcri(i) = Tv(i)
+            Pcri(i) = Pv(i)
+            Dcri(i) = Dv(i)
+        end do
+
+        n_cri = 3
+
+
+        ! Algorithm ends here
+        !-----------------------------------------------------------
+
+    end subroutine envelope
+end module envelope_sur
