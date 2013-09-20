a76720a9ca0c6def5dfe808f8804dc5bc2344aff
Martín Gaitán <mgaitan@machinalis.com>
2013-09-20 02:19:28 -0300

    using in memory database

--- sur/__init__.py
+++ sur/__init__.py
@@ -1,12 +1,35 @@
 import os
 import sys
+from StringIO import StringIO
+import sqlite3
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 DATA = os.path.abspath(os.path.join(ROOT, '..', 'data'))
 
 data = lambda a: os.path.join(DATA, a)
 
-if ROOT not in sys.path:
-    sys.path.append(ROOT)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+def setup_db():
+    if ROOT not in sys.path:
+        sys.path.append(ROOT)
+
+    import django.core.management
+    from django.core.management import call_command
+    import settings
+    django.core.management.setup_environ(settings)
+
+    # Read database to tempfile
+    con = sqlite3.connect(data('dev.db'))
+    tempfile = StringIO()
+    for line in con.iterdump():
+        tempfile.write('%s\n' % line)
+    con.close()
+    tempfile.seek(0)
+
+    from django.db import connection
+    # Use in memory and import from tempfile
+    connection.cursor().executescript(tempfile.read())
+
+    call_command('syncdb')
+
+setup_db()
--- sur/settings.py
+++ sur/settings.py
@@ -4,6 +4,11 @@ SECRET_KEY = '67eda6e0218611e38ff28c705af62198'
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': ':memory:' #data('dev.db'),
+    },
+  'dev':
+    {
+    'ENGINE': 'django.db.backends.sqlite3',
     'NAME': data('dev.db'),
     }
 }
