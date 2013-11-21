8d032363438af49593a57ce25c34b0c177cb71ef
Martín Gaitán <gaitan@gmail.com>
2013-11-21 17:21:28 -0300

    adding south migrations

--- sur/manage.py
+++ sur/manage.py
@@ -3,6 +3,6 @@ import sys
 import sur # NOQA
 
 if __name__ == "__main__":
-  sur.setup_as_lib()
+  # sur.setup_as_lib()
   from django.core.management import execute_from_command_line
   execute_from_command_line(sys.argv)
--- sur/settings.py
+++ sur/settings.py
@@ -1,3 +1,4 @@
+import sys
 from sur import data
 
 SECRET_KEY = '67eda6e0218611e38ff28c705af62198'
@@ -12,4 +13,8 @@ DATABASES = {
     'NAME': data('dev.db'),
     }
 }
-INSTALLED_APPS = ("sur", "django.contrib.auth", "django.contrib.contenttypes")
+
+INSTALLED_APPS = ["sur", "django.contrib.auth", "django.contrib.contenttypes"]
+
+if 'schemamigration' in sys.argv:
+    INSTALLED_APPS.append("south")
