45709aca79364df55e9fb308d8a813721bde5b12
Martín Gaitán <gaitan@gmail.com>
2014-12-15 09:18:08 -0300

    setting middlewares to avoid warning on import

--- sur/settings.py
+++ sur/settings.py
@@ -1,5 +1,4 @@
-import sys
-from sur import data
+
 
 SECRET_KEY = '67eda6e0218611e38ff28c705af62198'
 DATABASES = {
@@ -8,5 +7,12 @@ DATABASES = {
     'NAME': ':memory:'
     },
 }
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
 
 INSTALLED_APPS = ["sur", "django.contrib.auth", "django.contrib.contenttypes"]
